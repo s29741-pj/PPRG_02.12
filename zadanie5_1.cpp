dae3205d812e0f0f89fca3cfb219e3f9dcf88ce6
s29741-pj <s29741@pjwstk.edu.pl>
2023-12-06 00:42:38 +0100

    Initial commit

---
+++ zadanie5_1.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int vecSize = 0;
+    int vecElem = 0;
+    int counter = 0;
+    int biggestElem = 0;
+    int biggestElemIndex = 0;
+    vector<int> userVec;
+
+
+    cout << "Proszę podać liczbę elementów wektora: ";
+    cin >> vecSize;
+
+    while (counter < vecSize)
+    {
+        cout << "Proszę podać wartość " << counter + 1 << " elementu wektora: ";
+        cin >> vecElem;
+        userVec.push_back(vecElem);
+        counter++;
+    }
+
+
+    for(int i = 0; i < userVec.size(); i++){
+        if(userVec[i] > biggestElem){
+            biggestElem = userVec[i];
+            biggestElemIndex = i;
+        } 
+    }
+
+    cout << "Wartość elementu maksymalnego: " << biggestElem << endl;
+    cout << "Numer indeksu elementu maksymalnego: " << biggestElemIndex;
+
+    return 0;
+}
---
+++ zadanie5_2.cpp
@@ -0,0 +1,52 @@
+﻿#include<iostream>
+#include <iomanip>
+using namespace std;
+
+
+
+int main() {
+
+    int arrSize = 0;
+    int arrNum = 0;
+
+    cout << "Podaj długość tablicy : ";
+    cin >> arrSize;
+
+    int newArr[arrSize];
+
+
+    for (int i = 0; i < arrSize; i++) {
+            cout << "Podaj liczbę do umieszczenia w tablicy: ";
+            cin >> arrNum;
+            newArr[i] = arrNum;    
+     }
+
+    cout << endl;
+    cout << "Tablica wejściowa:"<< endl;
+    cout << endl;
+
+     for (int col = 0; col < arrSize; col++){
+        if(col % 2 == 0){
+            cout<< newArr[col] << setw(7);
+        }else if (col % 2 != 0){
+            cout<< newArr[col];
+            cout << endl;
+        }
+     }
+
+    cout << endl;
+    cout << "Tablica wyjściowa:"<< endl;
+    cout << endl;
+
+     for(int rev = 0; rev < arrSize; rev++){
+        
+        if(rev % 2 == 0){
+                cout<< newArr[rev+1] << setw(7);
+        }else if (rev % 2 != 0){
+                cout << newArr[rev-1];
+                cout << endl;
+          }  
+     }
+
+    return 0;
+}
